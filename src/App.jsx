bb922f4f45b98a9100982b1b676fc04f753736b5
Aminul Haqur Rana <aminulhaquerana1@gmail.com>
2024-10-13 18:58:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Profile from "../components/Profile";
+function App() {
+  return (
+    <div>
+      <Profile></Profile>
+      <Profile></Profile>
+      <Profile></Profile>
+      <Profile></Profile>
+      <h1>Lets Explore React</h1>
+    </div>
+  );
+}
+
+export default App;
